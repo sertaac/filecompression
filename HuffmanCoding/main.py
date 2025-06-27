8a4e60d959fb4e1b4e524048299ff81b8fa60e80
Sertaç <sertacataac@gmail.com>
2025-06-27 16:13:37 +0300

    Update main.py

--- HuffmanCoding/main.py
+++ HuffmanCoding/main.py
@@ -1,1 +1,13 @@
+from typing import Optional
 
+class HuffmanTree:
+    def __init__(self, value: Optional[str] = None, frequency: int = 0):
+        self.value: Optional[str] = value
+        self.frequency: int = frequency
+        self.left_child: Optional[HuffmanTree] = None
+        self.right_child: Optional[HuffmanTree] = None
+
+    def is_leaf(self) -> bool:
+        return self.value is not None
+    
+    
